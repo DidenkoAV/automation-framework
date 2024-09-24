6dcf987fcad45d4339bda6a6e45dc983e50721c2
DidenkoAV <42097989+DidenkoAV@users.noreply.github.com>
2024-09-23 21:45:01 -0400

    Update smoke.groovy

--- pipelines/smoke.groovy
+++ pipelines/smoke.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/DidenkoAV/automation-framework.git', credentialsId: '444ee57f-9f53-405e-8165-cb39adb89254'
+                git url: 'git@github.com:DidenkoAV/automation-framework.git', credentialsId: '2c3f7cc8-11a5-495b-a9f4-a45a06063136'
             }
         }
 
